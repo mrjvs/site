040b5f89eec80801ada1294797dbac68fecba31f
Jelle van Snik <jellevs@gmail.com>
2022-03-15 23:50:56 +0100

    categories type

--- components/typography/Paragraph.vue
+++ components/typography/Paragraph.vue
@@ -1,3 +1,9 @@
 <template>
-  <p class="mb-6 leading-6 paragraph"><slot /></p>
+  <p class="mb-6 leading-6.5 paragraph"><slot /></p>
 </template>
+
+<style scoped>
+.leading-6\.5 {
+  line-height: 1.6rem;
+}
+</style>
