d1e7640ef2fa7bfd4eb668ee012d85ebb8b11e51
mrjvs <mistrjvs@gmail.com>
2024-09-27 01:06:24 +0200

    Base colors

--- src/components/layouts/Content.tsx
+++ src/components/layouts/Content.tsx
@@ -1,18 +1,16 @@
 import { Container } from './Container';
 
 export type ContentProps = {
-  navigation: () => any;
+  navigation: any;
   children: any;
 };
 
 export function Content(props: ContentProps) {
-  const Navigation = props.navigation ?? (() => <></>);
+  const nav = () => props.navigation ?? null;
 
   return (
     <>
-      <Container>
-        <Navigation />
-      </Container>
+      <Container>{nav()}</Container>
       <Container>{props.children}</Container>
     </>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shade: {
+          800: '#0A0C0B',
+          400: '#9FAAA5',
+          300: '#EFDCC3',
+        }
+      }
+    },
   },
   plugins: [],
 };
