d50df826375fc6e717c2c632520e2d4443510c47
mrjvs <mistrjvs@gmail.com>
2024-10-06 23:32:37 +0200

    Modify first paragraph

--- src/views/HeroView.tsx
+++ src/views/HeroView.tsx
@@ -14,9 +14,8 @@ export function HeroView() {
       </HeroHeading>
       <Paragraph class="mb-8">
         Hey, I am mrjvs. I am a full-stack web developer who likes to zoom in on
-        UI design to make the perfect package.
-        <br />I dabble in open source whenever I see a gap in good tooling or
-        software.
+        UI design to make the perfect package. I dabble in open source whenever
+        I see a gap in good tooling or software - it's fun!
       </Paragraph>
       <div class="flex items-center gap-8">
         <SocialLink icon="email" to="mailto:hello@mrjvs.com">
