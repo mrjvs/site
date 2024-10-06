f630fa9ac8292ad2ad363caa28db497f7f9b43ed
mrjvs <mistrjvs@gmail.com>
2024-10-06 23:43:13 +0200

    Responsiveness

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -9,9 +9,9 @@ export type BreakoutProps = {
 };
 
 export function Container(props: ContainerProps) {
-  return <div class="max-w-[600px] w-full mx-auto">{props.children}</div>;
+  return <div class="max-w-[600px] px-8 w-full mx-auto">{props.children}</div>;
 }
 
 export function Breakout(props: BreakoutProps) {
-  return <div class="-mx-10">{props.children}</div>;
+  return <div class="-mx-4">{props.children}</div>;
 }
--- src/components/layouts/StampedCard.tsx
+++ src/components/layouts/StampedCard.tsx
@@ -8,7 +8,7 @@ export function StampedCard(props: StampedCardProps) {
   return (
     <div class="mb-4 relative group/card transition-transform hover:-translate-y-1 overflow-hidden bg-shade-600 rounded-xl p-7">
       <div class="absolute top-0 inset-x-0 bg-gradient-to-b group-hover/card:opacity-100 opacity-0 transition-opacity duration-300 from-shade-550 to-transparent h-24 pointer-events-none" />
-      <div class="absolute top-6 right-6 grid gap-2 grid-cols-2 grid-rows-2">
+      <div class="absolute top-4 sm:top-6 right-4 sm:right-6 grid gap-2 grid-cols-2 grid-rows-2">
         <div class="h-1 w-1 bg-shade-700" />
         <div class="h-1 w-1 bg-primary-100" />
         <div class="h-1 w-1 col-start-2 bg-shade-700" />
