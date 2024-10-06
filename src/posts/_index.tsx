6e37b7c0597681fdc66154f6178dc39110a9f895
mrjvs <mistrjvs@gmail.com>
2024-10-06 18:20:19 +0200

    Remove props

--- src/posts/_index.tsx
+++ src/posts/_index.tsx
@@ -4,8 +4,8 @@ import * as HelloWorld from './hello-world.mdx';
 import * as EmailGarbage from './email-garbage.mdx';
 import * as PrioritizeStructure from './prioritize-structure.mdx';
 
-export const posts = makePostCollection((props) => {
-  props.addMdx(HelloWorld);
-  props.addMdx(EmailGarbage);
-  props.addMdx(PrioritizeStructure);
+export const posts = makePostCollection(({ addMdx }) => {
+  addMdx(HelloWorld);
+  addMdx(EmailGarbage);
+  addMdx(PrioritizeStructure);
 });
