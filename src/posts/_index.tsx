158d65221b61d6dfcceef8d11fae6b605462815f
mrjvs <mistrjvs@gmail.com>
2024-10-06 20:31:17 +0200

    Remove blog section of website

--- src/posts/_index.tsx
+++ src/posts/_index.tsx
@@ -1,11 +1,7 @@
 import { makePostCollection } from '~/utils/post-collection';
 
 import * as HelloWorld from './hello-world.mdx';
-import * as EmailGarbage from './email-garbage.mdx';
-import * as PrioritizeStructure from './prioritize-structure.mdx';
 
 export const posts = makePostCollection(({ addMdx }) => {
   addMdx(HelloWorld);
-  addMdx(EmailGarbage);
-  addMdx(PrioritizeStructure);
 });
